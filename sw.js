17a1f76373464df4572cd8d5ecc15daf020557c0
Mikhail Yudin <yumih@r-o.ru>
2019-08-22 20:22:08 +0700

    fix offline page, accessibility

--- sw.js
+++ sw.js
@@ -6,14 +6,14 @@ sitemap: false
 // Licensed under a CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
 // http://creativecommons.org/publicdomain/zero/1.0/
 
-(function() {
+(function () {
 
   // Update 'version' if you need to refresh the cache
-  var staticCacheName = 'static';
-  var version = 'v1::';
+  var staticCacheName = 'static'
+  var version = 'v2::'
 
   // Store core files in a cache (including a page to display when offline)
-  function updateStaticCache() {
+  function updateStaticCache () {
     return caches.open(version + staticCacheName)
       .then(function (cache) {
         return cache.addAll([
@@ -21,13 +21,13 @@ sitemap: false
           '/assets/css/main.css',
           '/',
           '/offline.html'
-        ]);
-      });
-  };
+        ])
+      })
+  }
 
   self.addEventListener('install', function (event) {
-    event.waitUntil(updateStaticCache());
-  });
+    event.waitUntil(updateStaticCache())
+  })
 
   self.addEventListener('activate', function (event) {
     event.waitUntil(
@@ -36,27 +36,27 @@ sitemap: false
           // Remove caches whose name is no longer valid
           return Promise.all(keys
             .filter(function (key) {
-              return key.indexOf(version) !== 0;
+              return key.indexOf(version) !== 0
             })
             .map(function (key) {
-              return caches.delete(key);
+              return caches.delete(key)
             })
-          );
+          )
         })
-    );
-  });
+    )
+  })
 
   self.addEventListener('fetch', function (event) {
-    var request = event.request;
+    var request = event.request
     // Always fetch non-GET requests from the network
     if (request.method !== 'GET') {
       event.respondWith(
         fetch(request)
           .catch(function () {
-            return caches.match('/offline.html');
+            return caches.match('/offline.html')
           })
-      );
-      return;
+      )
+      return
     }
 
     // For HTML requests, try the network first, fall back to the cache, finally the offline page
@@ -69,27 +69,27 @@ sitemap: false
           mode: request.mode,
           credentials: request.credentials,
           redirect: request.redirect
-        });
+        })
       }
       event.respondWith(
         fetch(request)
           .then(function (response) {
             // Stash a copy of this page in the cache
-            var copy = response.clone();
+            var copy = response.clone()
             caches.open(version + staticCacheName)
               .then(function (cache) {
-                cache.put(request, copy);
-              });
-            return response;
+                cache.put(request, copy)
+              })
+            return response
           })
           .catch(function () {
             return caches.match(request)
               .then(function (response) {
-                return response || caches.match('/offline.html');
+                return response || caches.match('/offline.html')
               })
           })
-      );
-      return;
+      )
+      return
     }
 
     // For non-HTML requests, look in the cache first, fall back to the network
@@ -100,11 +100,11 @@ sitemap: false
             .catch(function () {
               // If the request is for an image, show an offline placeholder
               if (request.headers.get('Accept').indexOf('image') !== -1) {
-                return new Response('<svg width="400" height="300" role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', { headers: { 'Content-Type': 'image/svg+xml' }});
+                return new Response('<svg width="400" height="300" role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', { headers: { 'Content-Type': 'image/svg+xml' } })
               }
-            });
+            })
         })
-    );
-  });
+    )
+  })
 
-})();
+})()
