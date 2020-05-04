fa0774f994ea91c6c47005e445184581126e67e2
fagcinsk <fagci.nsk@gmail.com>
2020-05-04 19:07:04 +0700

    revert headers

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,8 @@
     window.scrollTo(0, 0)
   })
 
-  function onScroll (e) {
-    if(window.scrollY > maxScroll === isWasShown) return;
+  function onScroll(e) {
+    if (window.scrollY > maxScroll === isWasShown) return
     isWasShown = !isWasShown
     if (isWasShown) {
       goTop.classList.add('show')
@@ -20,7 +20,7 @@
 
   window.addEventListener('scroll', onScroll, {
     capture: true,
-    passive: true
+    passive: true,
   })
 
   requestAnimationFrame(onScroll)
@@ -28,6 +28,6 @@
   if (!d.body.querySelector('.codehilite')) return
   const link = d.createElement('link')
   link.rel = 'stylesheet'
-  link.href = '//cdn.jsdelivr.net/npm/pygments-css@1.0.0/default.css'
+  link.href = '/assets/css/pygments-default.css'
   d.getElementsByTagName('head')[0].appendChild(link)
 })(window)
