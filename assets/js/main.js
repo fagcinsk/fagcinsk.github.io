c6a0c45336a8110f0512ba84fc4680a97ebdebd4
fagcinsk <fagci.nsk@gmail.com>
2020-06-27 22:00:30 +0700

    mobile menu fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,30 @@
 (function (window) {
-  const d = window.document,
-    goTop = d.querySelector('#goTop'),
-    maxScroll = window.innerHeight / 2
-  let isWasShown = false
+    const d = window.document,
+        goTop = d.querySelector('#goTop'),
+        maxScroll = window.innerHeight / 2
+    let isWasShown = false
 
-  goTop.addEventListener('click', function () {
-    window.scrollTo(0, 0)
-  })
+    goTop.addEventListener('click', function () {
+        window.scrollTo(0, 0)
+    })
 
-  function onScroll(e) {
-    if (window.scrollY > maxScroll === isWasShown) return
-    isWasShown = !isWasShown
-    if (isWasShown) {
-      goTop.classList.add('show')
-    } else {
-      goTop.classList.remove('show')
+    function onScroll() {
+        if (window.scrollY > maxScroll === isWasShown) return
+        isWasShown = !isWasShown
+        if (isWasShown) {
+            goTop.classList.add('show')
+        } else {
+            goTop.classList.remove('show')
+        }
     }
-  }
 
-  window.addEventListener('scroll', onScroll, {
-    capture: true,
-    passive: true,
-  })
+    window.addEventListener('scroll', onScroll, {passive: true})
 
-  requestAnimationFrame(onScroll)
+    requestAnimationFrame(onScroll)
 
-  if (!d.body.querySelector('.codehilite')) return
-  const link = d.createElement('link')
-  link.rel = 'stylesheet'
-  link.href = '/assets/css/pygments-default.css'
-  d.getElementsByTagName('head')[0].appendChild(link)
+    if (!d.body.querySelector('.codehilite')) return
+    const link = d.createElement('link')
+    link.rel = 'stylesheet'
+    link.href = '/assets/css/pygments-default.css'
+    d.getElementsByTagName('head')[0].appendChild(link)
 })(window)
